417bbfa94a1660458fe0f9180533b10c512abbb4
Todd Wongkee <twongkee@gmail.com>
2023-06-04 11:00:00 -0400

    constants

---
+++ mobian/pygame/constants.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 20 07:46:25 2017
+
+@author: Todd Wongkee
+"""
+from pygame.locals import *
+
+# set up the colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREY = (127, 127, 127)
+DARKGREY = (31, 31, 31)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+LIGHTBLUE = (150, 198, 250)
+FPS = 30
+
+# Screen dimensions
+SCREEN_WIDTH = 320
+SCREEN_HEIGHT = 240
+PLAYER_SIZE = 16
+WALL_SIZE = 16
+STAR_SIZE = 16
+COIN_SIZE = 16
+STEP_SIZE = 16
+
+GK_UP = K_UP
+GK_DOWN = K_DOWN
+GK_LEFT = K_LEFT
+GK_RIGHT = K_RIGHT
+GK_MENU = K_ESCAPE
+GK_X = K_u
+GK_Y = K_i
+GK_A = K_j
+GK_B = K_k
+GK_SELECT = K_SPACE
+GK_START = K_RETURN
+
+
+
+# plsyer
+SPEED = 5
+JUMPSIZE = 3
